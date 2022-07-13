40bb9c75f3c7085c56454c42ecadcb590daa1615
Tiarethorns <tcarolinaespinoza@gmail.com>
2022-07-13 13:22:45 -0400

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,68 @@
+//FÓRMULA E INDICACIONES PARA CIFRAR
 const cipher = {
-  // ...
-};
-
-export default cipher;
+    encode: (offset, string,) => {
+      let resultado = "";
+      let mensaje = "";
+      for (let i = 0; i < string.length; i++) {
+        //Fórmula letras mayúsculas
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          mensaje = ((string.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65;
+        }
+  
+        //Fórmula letras minúsculas
+        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+          mensaje = ((string.charCodeAt(i) - 97 + parseInt(offset)) % 26) + 97;
+        }
+  
+        //Espacio en el Número 32 del Código ASCII
+        else if (string.charCodeAt(i) === 32) {
+          mensaje = 32;
+        }
+  
+        //Agregar números del Código ASCII
+        else if (string.charCodeAt(i) >= 48 && string.charCodeAt(i) <= 57) {
+          mensaje = ((string.charCodeAt(i) - 57 + parseInt(offset)) % 10) + 57;
+        }
+  
+        resultado += String.fromCharCode(mensaje);
+      }
+  
+      return resultado;
+  
+    },
+  
+  
+    //FÓRMULA E INDICACIONES PARA DESCIFRAR
+    decode: (offset, string) => {
+      let resultado = "";
+      let mensaje = "";
+      for (let i = 0; i < string.length; i++) {
+        //Fórmula letras mayúsculas
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          mensaje = ((string.charCodeAt(i) - 90 - parseInt(offset)) % 26) + 90;
+        }
+  
+        //Fórmula letras minúsculas
+        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+          mensaje = ((string.charCodeAt(i) - 122 - parseInt(offset)) % 26) + 122;
+        }
+  
+        //Espacio en el Número 32 del Código ASCII
+        else if (string.charCodeAt(i) === 32) {
+          mensaje = 32;
+        }
+  
+        //Agregar números del Código ASCII
+        else if (string.charCodeAt(i) >= 48 && string.charCodeAt(i) <= 57) {
+          mensaje = ((string.charCodeAt(i) - 57 - parseInt(offset)) % 10) + 57;
+        }
+  
+        resultado += String.fromCharCode(mensaje);
+      }
+  
+      return resultado;
+  
+    }
+  }
+  
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import cipher from './cipher.js';
+//PÁGINAS PRINCIPALES CON LA FUNCIÓN DE OCULTAR Y MOSTRAR
+document.getElementById('pag1').style.display='block';
+document.getElementById('pag2').style.display='none';
+
+//INDICACIÓN DE QUE AL MOMENTO DE CLICKEAR COMENZAR LA PÁGINA UNO DESAPAREZCA
+let comenzar=document.getElementById('comenzar');
+comenzar.addEventListener('click', pagina2);
+function pagina2(){
+    document.getElementById('pag1').style.display='none';
+    document.getElementById('pag2').style.display='block';
+}
+//INDICAIÓN PARA VOLVER A LA PÁGINA 1 PRESIONANDO EL LOGO
+let PaginaInicio=document.getElementById('logo');
+PaginaInicio.addEventListener('click', volver);
+function volver(){
+   document.getElementById('pag1').style.display='block';
+   document.getElementById('pag2').style.display='none';
+}
+//LLAMAR FUNCIONES CIFRAR, DESCIFRAR Y RESULTADO, Y UNIENDOLO CON CIPHER.JS
+document.getElementById('cifrar').addEventListener('click', () => {
+    let string = document.getElementById('mensaje').value;
+    let offset = document.getElementById('desplazar').value;
+    document.getElementById('resultado').value = cipher.encode(offset, string);
+    console.log(cipher.encode(offset, string));
+})
+
+document.getElementById('descifrar').addEventListener('click', () => {
+    let string = document.getElementById('mensaje').value;
+    let offset = document.getElementById('desplazar').value;
+    document.getElementById('resultado').value = cipher.decode(offset, string);
+   
+})
+
+document.getElementById('resultado').addEventListener(('click'), () => {
+  let string = document.getElementById('mensaje').value;
+  let offset = document.getElementById('desplazar').value;
+  document.getElementById('resultado').value = cipher(offset, string);
+})
 
 console.log(cipher);
